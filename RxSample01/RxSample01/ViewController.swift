da8f218ee6869f56db192950ba0b83effe27669f
KazuyoshiUeno <ios.studio23c@gmail.com>
2015-12-25 01:45:29 +0900

    do not anything after onError occuered.

--- RxSample01/RxSample01/ViewController.swift
+++ RxSample01/RxSample01/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField02: UITextField!
+    @IBOutlet weak var dispatchBtn: UIButton!
+    @IBOutlet weak var textField03: UITextField!
     //
     // MARK: - variables -
     //
@@ -40,7 +42,7 @@ class ViewController: UIViewController {
     private func sample() {
         
         // 基本的な振る舞い
-        ["hello", "world"].asObservable().subscribeNext({ str -> Void in print(str) })
+        ["hello", "world"].toObservable().subscribeNext({ str -> Void in print(str) }).addDisposableTo(disposeBag)
         
         // UIButtnのタップ取得
         myButton.rx_tap.subscribeNext({ print("btn tapped") }).addDisposableTo(disposeBag)
@@ -54,6 +56,29 @@ class ViewController: UIViewController {
         slider.rx_value
             .map({ String($0) })
         .bindTo(textField02.rx_text)
+        .addDisposableTo(disposeBag)
+        
+        // エラーの伝搬をしたい時も便利
+        //  エラーの取り回しを例外を使わずに行うことができる
+        //  / flatMap内でjust(値）を返せば成功値を伝搬させることができる
+        //  / flatMapないでfailWith(NSError)を返せばエラーを伝搬させることができる
+        dispatchBtn.rx_tap.flatMap { () -> Observable<String> in
+            print("flat")
+            if (self.textField03.text?.characters.count > 0) ?? false {
+                return just(String(self.textField03.text))
+            } else {
+                return failWith(NSError(domain: "app error", code: -1, userInfo: nil))
+            }
+        }
+
+        //.catchErrorJustReturn("5")
+
+        .subscribe(
+            onNext: { print("値は\($0)") },
+            onError: { _ in print("") },
+            onCompleted: nil,
+            onDisposed: nil
+        )
         .addDisposableTo(disposeBag)
     }
 
